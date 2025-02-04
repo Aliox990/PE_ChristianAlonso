9018fe9220e26928822d2bca5cf9b74d2d70548d
Christian Alonso <christian.arevalos8710@alumnos.udg.mx>
2025-02-04 10:25:48 -0600

    Subiendo primeros archivos

---
+++ Introduccion/presentacion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/*Inicio de main*/
+int main() {
+    printf ("Nombre: Christian Alonso Arevalos Gonzalez\n");
+    printf ("Edad: 19\n");
+    printf ("Lugar de residencia: Tepatitlan de Morelos, Jalisco\n");
+    printf ("Fecha: 30/01/2025\n");
+    printf ("Carrera: Ingenieria en computacion\n");
+    printf ("Semestre: Segundo\n");
+    return 0;
+}// Fin de main
